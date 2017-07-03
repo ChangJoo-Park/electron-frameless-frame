87072eedfbedd1e5bf77cc537cea0b9c9c624787
ChangJoo Park <pcjpcj2@gmail.com>
2017-07-03 10:19:40 +0900

    update tests

--- test/renderer.js
+++ test/renderer.js
@@ -1,6 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-FramelessFrame({
+const remote = require('electron').remote
+
+FramelessFrame(remote, {
   theme: 'ff-default'
 })
